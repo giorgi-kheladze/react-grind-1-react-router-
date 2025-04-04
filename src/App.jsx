f36026ac68cbbcaf578474c1782b4f22ff251cef
Giorgi Kheladze <giorgikheladze546@gmail.com>
2025-04-05 00:14:12 +0400

    new method

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import RootLayout from "./layout/RootLayout";
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Route>
+    )
+  );
   return (
     <div>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/product" element={<Product />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/about" element={<About />}></Route>
-        </Routes>
-      </div>
+      <RouterProvider router={router} />
     </div>
   );
 };
